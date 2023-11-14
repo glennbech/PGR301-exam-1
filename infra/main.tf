4f2c0de00be5a6af819dd28488c38dcbc7cc26c9
jalvsaker <jalvsaker@users.noreply.github.com>
2023-11-14 12:58:36 +0000

    terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 resource "aws_apprunner_service" "service" {
-  service_name = "kjell-is-king"
+  service_name = var.prefix
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
@@ -13,7 +13,7 @@ resource "aws_apprunner_service" "service" {
       image_configuration {
         port = "8080"
       }
-      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
+      image_identifier      = var.image
       image_repository_type = "ECR"
     }
     auto_deployments_enabled = true
@@ -21,7 +21,7 @@ resource "aws_apprunner_service" "service" {
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "kjell-role-thingy"
+  name               = "${var.prefix}-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -45,14 +45,14 @@ data "aws_iam_policy_document" "policy" {
     actions   = ["rekognition:*"]
     resources = ["*"]
   }
-  
-  statement  {
+
+  statement {
     effect    = "Allow"
     actions   = ["s3:*"]
     resources = ["*"]
   }
 
-  statement  {
+  statement {
     effect    = "Allow"
     actions   = ["cloudwatch:*"]
     resources = ["*"]
@@ -60,7 +60,7 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "kjell-apr-policy-thingy"
+  name        = "${var.prefix}-apr-policy-thingy"
   description = "Policy for apprunner instance I think"
   policy      = data.aws_iam_policy_document.policy.json
 }
